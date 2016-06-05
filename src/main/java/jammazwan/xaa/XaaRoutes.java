8350a882f784480e4fcd24903e2a8f1b776b4d30
petecarapetyan <pete@couldbe.net>
2016-06-04 20:00:30 -0500

    completed project

--- src/main/java/jammazwan/xaa/XaaRoutes.java
+++ src/main/java/jammazwan/xaa/XaaRoutes.java
@@ -1,25 +1,16 @@
 package jammazwan.xaa;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
-
 
 public class XaaRoutes extends RouteBuilder {
 
-    @Autowired private XaaBean xaaBean;
-    
+	@Autowired
+	private XaaProcessor xaaProcessor;
 
-    @Override
-    public void configure() throws Exception {
-        from("direct:xaa")
-            .process(new Processor() {
-                public void process(Exchange exchange) throws Exception {
-                    String text = exchange.getIn().getBody(String.class);
-                    String newAnswer = xaaBean.answer(text);
-                    exchange.getOut().setBody(newAnswer);
-                }
-            });
-    }
+	@Override
+	public void configure() throws Exception {
+		from("direct:xaa").marshal().json(JsonLibrary.Jackson, true).process(xaaProcessor).to("file:.");
+	}
 }
--- src/test/java/jammazwan/xaa/XaaTest.java
+++ src/test/java/jammazwan/xaa/XaaTest.java
@@ -5,17 +5,21 @@ import org.junit.Test;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import jammazwan.City;
+import jammazwan.raw.CityPojo;
+
 public class XaaTest extends CamelSpringTestSupport {
 
-    @Override
-    protected AbstractXmlApplicationContext createApplicationContext() {
-        return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
-    }
+	@Override
+	protected AbstractXmlApplicationContext createApplicationContext() {
+		return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
+	}
 
-    @Test
-    public void testHelloBean() throws Exception {
-        String reply = template.requestBody("direct:xaa", "No Meaning Here", String.class);
-		assertEquals("My No Meaning Here", reply);
-    }
+	@Test
+	public void testCityToJson() throws Exception {
+		City city = new CityPojo(1, "Austin, TX", "USA", 1234567, 123, 12345);
+		String reply = template.requestBody("direct:xaa", city, String.class);
+		assertTrue(reply.contains("\"city\" : \"Austin, TX\""));
+	}
 
 }
